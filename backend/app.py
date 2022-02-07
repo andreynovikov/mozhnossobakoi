036016277bf7ccfffd7cff4b85418914d60f6b44
Andrey Novikov <novikov@gmail.com>
2022-02-07 03:21:15 +0300

    Add logging (temporary)

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
+import logging
+
 from flask import Flask, g
 from peewee import PostgresqlDatabase
 
+
+logger = logging.getLogger('peewee')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object('config.Configuration')
