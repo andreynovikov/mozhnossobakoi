7617a97e2706604fd2b40144cf72a461a0c38384
Andrey Novikov <novikov@gmail.com>
2022-02-25 17:10:53 +0300

    Visualize loading state

---
+++ src/PlaceCard.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Skeleton from '@mui/material/Skeleton';
+import Typography from '@mui/material/Typography';
+
+import { SocialIcon } from 'react-social-icons';
+
+import moment from 'moment';
+import 'moment/locale/ru';
+
+import PlaceIcon from './PlaceIcon';
+import PostalAddress from './PostalAddress';
+import { formatPhoneNumber } from './utils';
+
+
+export default function PlaceCard({place, mobile, onOpenPlace, onShowLocation, loading=false}) {
+    return (
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Stack direction="row" justifyContent="space-between" alignItems="flex-start" spacing={1} sx={{ mb: 1 }}>
+            <Stack direction="row" alignItems="baseline" spacing={0.5}>
+              {loading ? (
+                <Skeleton variant="circular" width={25} height={25}><PlaceIcon fontSize="small" /></Skeleton>
+              ) : (
+                <PlaceIcon kind={place.kind} fontSize="small" color={place.claimed ? "success" : "primary"} style={{ verticalAlign: "text-top" }} />
+              )}
+              <Typography variant="h6" component="div">
+                {loading ? <Skeleton width="9em" /> : place.name}
+              </Typography>
+            </Stack>
+            {(loading || (place.last_seen && moment(place.last_seen).isAfter('0001-01-01'))) && (
+              <Typography sx={{ mb: 1.5, textAlign: "end" }} variant="caption" color="text.secondary">
+                { loading ? <Skeleton width="5em" /> :  moment(place.last_seen).format('MMMM YYYY')}
+              </Typography>
+            )}
+          </Stack>
+          <Stack direction="column" spacing={0.5}>
+            {loading ? (
+              <React.Fragment>
+                <Skeleton height={15} width="80%" />
+                <Skeleton height={15} width="40%" />
+                <Skeleton />
+                <Skeleton width="85%" />
+                <Skeleton width="90%" />
+                <Skeleton height={12} width="30%" />
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {place.address && <Typography variant="body2"><PostalAddress address={place.address} /></Typography>}
+                {place.phone && <Typography variant="body2">{formatPhoneNumber(place.phone)}</Typography>}
+                {place.claim && <Typography variant="body1">{place.claim}</Typography>}
+                {place.url && <Link href={place.url} target="_blank" variant="caption">{place.url.replace(/https?:\/\//, '')}</Link>}
+              </React.Fragment>
+            )}
+          </Stack>
+
+        </CardContent>
+        <Stack direction="row" justifyContent="space-between" alignItems="flex-end" spacing={0.5}>
+          <CardActions disableSpacing sx={{ flexWrap: "wrap" }}>
+            {loading ? (
+              <React.Fragment>
+                <Skeleton height={25} width="6em" sx={{ mx: 1 }} />
+                <Skeleton height={25} width="8em" sx={{ mx: 1 }} />
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <Button size="small" onClick={() => onOpenPlace(place.id)}>Подробнее</Button>
+                <Button size="small" onClick={() => onShowLocation(place.position)}>Показать&nbsp;на&nbsp;карте</Button>
+              </React.Fragment>
+            )}
+          </CardActions>
+          <Stack direction="row" sx={{ flexWrap: "wrap", justifyContent: "flex-end", p: 0.5 }}>
+            {place.vk && <SocialIcon url={place.vk} target="_blank" style={{ height: 22, width: 22, margin: 2 }} />}
+            {place.facebook && <SocialIcon url={place.facebook} target="_blank" style={{ height: 22, width: 22, margin: 2 }} />}
+            {place.telegram && <SocialIcon url={`https://telegram.me/${place.telegram}`} target="_blank" style={{ height: 22, width: 22, margin: 2 }} />}
+            {place.instagram && <SocialIcon url={`https://instagram.com/${place.instagram}`} target="_blank" style={{ height: 22, width: 22, margin: 2 }} />}
+          </Stack>
+        </Stack>
+      </Card>
+    );
+}
+
+PlaceCard.propTypes = {
+    loading: PropTypes.bool,
+};
--- src/PlacesList.js
+++ src/PlacesList.js
@@ -5,33 +5,19 @@ import { useLocation, useSearchParams } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CircularProgress from '@mui/material/CircularProgress';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 import Masonry from '@mui/lab/Masonry';
-
-import { SocialIcon } from 'react-social-icons';
-
-import moment from 'moment';
-import 'moment/locale/ru';
 
 import {
     placeKeys,
     loadPlaces,
 } from './queries';
 
+import PlaceCard from './PlaceCard';
 import PlaceDrawer from './PlaceDrawer';
 import PlaceFilter from './PlaceFilter';
-import PlaceIcon from './PlaceIcon';
-import PostalAddress from './PostalAddress';
 import { useDocumentTitle } from './hooks';
-import { formatPhoneNumber } from './utils';
 
 
 function humanizeAction(action) {
@@ -132,49 +118,19 @@ export default function PlacesList({mobile, onShowLocation}) {
       <Box sx={{ p: 2, position: "relative", overflow: "auto" }}>
         <Box sx={{ p: 1 }}><PlaceFilter mobile={mobile} action={actionFilter} address={addressFilter} onFiltersChanged={onFiltersChanged}/></Box>
         <Typography gutterBottom variant={mobile ? "h5" : "h4"} component="h1" sx={{ px: 1, mt: 2 }}>{title}</Typography>
-          {isSuccess && data?.results ? <Masonry columns={{ m: 1, md: 2, lg: Math.max(2, Math.min(3, data.results.length)), xl: Math.max(2, Math.min(4, data.results.length)) }} spacing={0}>
-          {data.results.map((place, idx) =>
-            <Box key={place.id} sx={{ p: 1 }}>
-              <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                  <Stack direction="row" justifyContent="space-between" alignItems="flex-start" spacing={1} sx={{ mb: 1 }}>
-                    <Stack direction="row" alignItems="baseline" spacing={0.5}>
-                      <PlaceIcon kind={place.kind} fontSize="small" color={place.claimed ? "success" : "primary"} style={{ verticalAlign: "text-top" }} />
-                      <Typography variant="h6" component="div">
-                        {place.name}
-                      </Typography>
-                    </Stack>
-                    {place.last_seen && moment(place.last_seen).isAfter('0001-01-01') && <Typography sx={{ mb: 1.5, textAlign: "end" }} variant="caption" color="text.secondary">
-                      {moment(place.last_seen).format('MMMM YYYY')}
-                    </Typography>}
-                  </Stack>
-                  <Stack direction="column" spacing={0.5}>
-                    {place.address && <Typography variant="body2"><PostalAddress address={place.address} /></Typography>}
-                    {place.phone && <Typography variant="body2">{formatPhoneNumber(place.phone)}</Typography>}
-                    {place.claim && <Typography variant="body1">{place.claim}</Typography>}
-                    {place.url && <Link href={place.url} target="_blank" variant="caption">{place.url.replace(/https?:\/\//, '')}</Link>}
-                  </Stack>
-                </CardContent>
-                <Stack direction="row" justifyContent="space-between" alignItems="flex-end" spacing={0.5}>
-                  <CardActions disableSpacing sx={{ flexWrap: "wrap" }}>
-                    <Button size="small" onClick={() => onOpenPlace(place.id)}>Подробнее</Button>
-                    <Button size="small" onClick={() => onShowLocation(place.position)}>Показать&nbsp;на&nbsp;карте</Button>
-                  </CardActions>
-                  <Stack direction="row" sx={{ flexWrap: "wrap", justifyContent: "flex-end", p: 0.5 }}>
-                    {place.vk && <SocialIcon url={place.vk} target="_blank" style={{ height: 22, width: 22, margin: 2 }} />}
-                    {place.facebook && <SocialIcon url={place.facebook} target="_blank" style={{ height: 22, width: 22, margin: 2 }} />}
-                    {place.telegram && <SocialIcon url={`https://telegram.me/${place.telegram}`} target="_blank" style={{ height: 22, width: 22, margin: 2 }} />}
-                    {place.instagram && <SocialIcon url={`https://instagram.com/${place.instagram}`} target="_blank" style={{ height: 22, width: 22, margin: 2 }} />}
-                  </Stack>
-                </Stack>
-              </Card>
-            </Box>
-          )}
-        </Masonry>
-        :
-        <Box sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
-          <CircularProgress disableShrink />
-        </Box>}
+        {isFetching ? (
+          <Box sx={{ p: 1, maxWidth: { xs: "100%", md: "50%", lg: "33%", xl: "25%"} }}>
+            <PlaceCard place={{}} mobile={mobile} loading />
+          </Box>
+        ) : isSuccess && data?.results && (
+          <Masonry columns={{ xs: 1, md: 2, lg: Math.max(2, Math.min(3, data.results.length)), xl: Math.max(2, Math.min(4, data.results.length)) }} spacing={0}>
+            {data.results.map((place, idx) =>
+              <Box key={place.id} sx={{ p: 1 }}>
+                <PlaceCard place={place} mobile={mobile} onOpenPlace={onOpenPlace} onShowLocation={onShowLocation} />
+              </Box>
+            )}
+          </Masonry>
+        )}
         <PlaceDrawer ref={drawerRef} open={open} onClose={onClose} mobile={mobile} id={placeId} />
       </Box>
     );
