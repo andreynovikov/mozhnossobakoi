61a6f80bd9b3451254f8ad8657906754d046c68a
Andrey Novikov <novikov@gmail.com>
2022-03-21 14:16:45 +0300

    Wrap social icons on small screens

--- src/PlaceShareDialog.js
+++ src/PlaceShareDialog.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
+import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
@@ -55,58 +56,78 @@ export default function PlaceShareDialog({place, open, onClose}) {
       <Dialog fullWidth maxWidth="sm" open={open} onClose={onClose}>
         <DialogContent>
           <Stack direction="column" spacing={1}>
-            <Stack direction="row" spacing={1}>
-              <Tooltip title="ВК" arrow>
-                <VKShareButton url={shareUrl}>
-                  <VKIcon size={32} round />
-                </VKShareButton>
-              </Tooltip>
-              <Tooltip title="Одноклассники" arrow>
-                <OKShareButton url={shareUrl}>
-                  <OKIcon size={32} round />
-                </OKShareButton>
-              </Tooltip>
-              <Tooltip title="Facebook" arrow>
-                <FacebookShareButton url={shareUrl} quote={title}>
-                  <FacebookIcon size={32} round />
-                </FacebookShareButton>
-              </Tooltip>
-              <Tooltip title="Telegram" arrow>
-                <TelegramShareButton url={shareUrl} title={title}>
-                  <TelegramIcon size={32} round />
-                </TelegramShareButton>
-              </Tooltip>
-              <Tooltip title="WhatsApp" arrow>
-                <WhatsappShareButton url={shareUrl} title={title} separator=":: ">
-                  <WhatsappIcon size={32} round />
-                </WhatsappShareButton>
-              </Tooltip>
-              <Tooltip title="Viber" arrow>
-                <ViberShareButton url={shareUrl} title={title}>
-                  <ViberIcon size={32} round />
-                </ViberShareButton>
-              </Tooltip>
-              <Tooltip title="Twitter" arrow>
-                <TwitterShareButton url={shareUrl} title={title}>
-                  <TwitterIcon size={32} round />
-                </TwitterShareButton>
-              </Tooltip>
-              <Tooltip title="Живой журнал" arrow>
-                <LivejournalShareButton url={shareUrl} title={title} description={shareUrl}>
-                  <LivejournalIcon size={32} round />
-                </LivejournalShareButton>
-              </Tooltip>
-              <Tooltip title="Mail.ru" arrow>
-                <MailruShareButton url={shareUrl} title={title}>
-                  <MailruIcon size={32} round />
-                </MailruShareButton>
-              </Tooltip>
-              <Tooltip title="На почту" arrow>
-                <EmailShareButton url={shareUrl} subject={title}>
-                  <EmailIcon size={32} round />
-                </EmailShareButton>
-              </Tooltip>
-            </Stack>
+            <Grid container spacing={0.5}>
+              <Grid item>
+                <Tooltip title="ВК" arrow>
+                  <VKShareButton url={shareUrl}>
+                    <VKIcon size={32} round />
+                  </VKShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Одноклассники" arrow>
+                  <OKShareButton url={shareUrl}>
+                    <OKIcon size={32} round />
+                  </OKShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Facebook" arrow>
+                  <FacebookShareButton url={shareUrl} quote={title}>
+                    <FacebookIcon size={32} round />
+                  </FacebookShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+               <Tooltip title="Telegram" arrow>
+                  <TelegramShareButton url={shareUrl} title={title}>
+                    <TelegramIcon size={32} round />
+                  </TelegramShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="WhatsApp" arrow>
+                  <WhatsappShareButton url={shareUrl} title={title} separator=":: ">
+                    <WhatsappIcon size={32} round />
+                  </WhatsappShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Viber" arrow>
+                  <ViberShareButton url={shareUrl} title={title}>
+                    <ViberIcon size={32} round />
+                  </ViberShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Twitter" arrow>
+                  <TwitterShareButton url={shareUrl} title={title}>
+                    <TwitterIcon size={32} round />
+                  </TwitterShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Живой журнал" arrow>
+                  <LivejournalShareButton url={shareUrl} title={title} description={shareUrl}>
+                    <LivejournalIcon size={32} round />
+                  </LivejournalShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Mail.ru" arrow>
+                  <MailruShareButton url={shareUrl} title={title}>
+                    <MailruIcon size={32} round />
+                  </MailruShareButton>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="На почту" arrow>
+                  <EmailShareButton url={shareUrl} subject={title}>
+                    <EmailIcon size={32} round />
+                  </EmailShareButton>
+                </Tooltip>
+              </Grid>
+            </Grid>
             <TextField defaultValue={shareUrl} size="small" margin="dense" InputProps={{readOnly: true}} />
             {navigator && navigator.clipboard && <Button variant="contained" size="small" onClick={handleCopy} sx={{width: 'fit-content'}}>
               Скопировать ссылку
