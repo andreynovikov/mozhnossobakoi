152351f67e15b260b621efb9465eb48a3e5abcdf
Andrey Novikov <novikov@gmail.com>
2023-03-02 18:48:26 +0300

    Add infinite scroll

--- src/PlacesList.js
+++ src/PlacesList.js
@@ -1,10 +1,12 @@
-import { useState, useEffect, useRef } from 'react';
-import { useQuery } from 'react-query';
+import { Fragment, useState, useEffect, useRef } from 'react';
+import { useInfiniteQuery } from 'react-query';
 import { useLocation, useSearchParams } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 
 import ReactGA from 'react-ga4';
 
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 import Masonry from '@mui/lab/Masonry';
@@ -49,6 +51,7 @@ function humanizeAddress(address) {
 export default function PlacesList({mobile, onShowLocation}) {
     const [searchParams, setSearchParams] = useSearchParams();
     const [title, setTitle] = useState('');
+    const [notice, setNotice] = useState('');
     const [actionFilter, setActionFilter] = useState(searchParams.get('action'));
     const [addressFilter, setAddressFilter] = useState(searchParams.get('address'));
     const [open, setOpen] = useState(false);
@@ -64,33 +67,48 @@ export default function PlacesList({mobile, onShowLocation}) {
         setAddressFilter(searchParams.get('address'));
     }, [searchParams]);
 
-    const {data, isSuccess, isFetching} = useQuery(
+    const {data, isLoading, isSuccess, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery(
         placeKeys.list(actionFilter, addressFilter),
-        () => loadPlaces(actionFilter, addressFilter),
+        ({ pageParam = 1 }) => loadPlaces(actionFilter, addressFilter, pageParam),
         {
+            getPreviousPageParam: (firstPage) => firstPage.page > 1 ? firstPage.page - 1 : undefined,
+            getNextPageParam: (lastPage) => lastPage.page * 20 < lastPage.count ? lastPage.page + 1 : undefined,
             onError: (error) => {
                 console.error(error);
             }
         }
     );
 
+    const { ref: loadMoreRef } = useInView({
+        onChange: (inView) => inView && hasNextPage && fetchNextPage()
+    });
+
     useEffect(() => {
-        if (!isFetching && data?.results?.length === 0) {
+        if (!isLoading && data?.pages?.[0].results?.length === 0) {
             setTitle("К сожалению, нет мест, удовлетворяющих выбранным критериям. Попробуйте изменить фильтры.");
             return;
         }
         var title = 'Места';
+        var notice = 'Это все dogfriendly места';
         if (actionFilter) {
-            title += ', где ' + humanizeAction(actionFilter);
+            const action = humanizeAction(actionFilter);
+            title += ', где ' + action;
+            notice += ', где можно ' + action;
             if (!addressFilter)
                 title += ' там';
         }
-        if (addressFilter)
-            title += ' в ' + humanizeAddress(addressFilter);
-        title += ', куда можно с собакой';
-        setTitle(title);
+        if (addressFilter) {
+            const address = humanizeAddress(addressFilter);
+            title += ' в ' + address;
+            notice += ' в ' + address;
+        }
+        setTitle(title + ', куда можно с собакой');
+        setNotice(notice + ', которые нам известны.');
+    }, [actionFilter, addressFilter, isLoading, data]);
+
+    useEffect(() => {
         ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search, title: title }); // we place it here to send correct page title
-    }, [actionFilter, addressFilter, isFetching, data]);
+    }, [title, location]);
 
     const onFiltersChanged = (action, address) => {
         console.log('action changed:', action);
@@ -114,23 +132,40 @@ export default function PlacesList({mobile, onShowLocation}) {
         setOpen(false);
     };
 
+    const length = data?.pages ? data.pages.reduce((length, page) => { return length + page.results.length }, 0) : 0;
+
     return (
       <Box sx={{ p: 2, position: "relative", overflow: "auto" }}>
         <Box sx={{ p: 1 }}><PlaceFilter mobile={mobile} action={actionFilter} address={addressFilter} onFiltersChanged={onFiltersChanged}/></Box>
         <Typography gutterBottom variant={mobile ? "h5" : "h4"} component="h1" sx={{ px: 1, mt: 2 }}>{title}</Typography>
-        {isFetching ? (
+        {isLoading ? (
           <Box sx={{ p: 1, maxWidth: { xs: "100%", md: "50%", lg: "33%", xl: "25%"} }}>
             <PlaceCard place={{}} mobile={mobile} loading />
           </Box>
-        ) : isSuccess && data?.results && (
-          <Masonry columns={{ xs: 1, md: 2, lg: Math.max(2, Math.min(3, data.results.length)), xl: Math.max(2, Math.min(4, data.results.length)) }} spacing={0}>
-            {data.results.map((place, idx) =>
-              <Box key={place.id} sx={{ p: 1 }}>
-                <PlaceCard place={place} mobile={mobile} onOpenPlace={onOpenPlace} onShowLocation={onShowLocation} />
-              </Box>
-            )}
+        ) : isSuccess && data?.pages && (
+          <Masonry columns={{ xs: 1, md: 2, lg: Math.max(2, Math.min(3, length)), xl: Math.max(2, Math.min(4, length)) }} spacing={0}>
+              {data.pages.map((page, pageIndex) =>
+                  <Fragment key={page.page}>
+                      {page.results.map((place, placeIndex) =>
+                          <Box key={place.id} ref={(pageIndex * 20 + placeIndex) === length - 1 ? loadMoreRef : undefined } sx={{ p: 1 }}>
+                              <PlaceCard place={place} mobile={mobile} onOpenPlace={onOpenPlace} onShowLocation={onShowLocation} />
+                          </Box>
+                      )}
+                  </Fragment>
+              )}
           </Masonry>
         )}
+        <Box sx={{ display: 'flex', justifyContent: 'center', pt: 5, pb: 1 }}>
+          {hasNextPage ? (
+            <Button variant="outlined" size="small" onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+              {isFetchingNextPage ? 'Загружаем ещё места...' : 'Загрузить больше мест'}
+            </Button>
+          ) : length > 0 ? (
+            <Typography gutterBottom variant="body2">
+              {notice}
+            </Typography>
+          ) : null}
+        </Box>
         <PlaceDrawer ref={drawerRef} open={open} onClose={onClose} mobile={mobile} id={placeId} onShowLocation={onShowLocation} />
       </Box>
     );
